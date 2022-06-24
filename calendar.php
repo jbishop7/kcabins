9852471716b1f91c865a04c75d40adcde72ad3e0
Jacob Bishop <71083701+jbishop7@users.noreply.github.com>
2022-06-23 21:09:38 -0400

    Add files via upload

---
+++ calendar.php
@@ -0,0 +1,136 @@
+
+<div id="content">
+
+
+
+<div align="center"><table width="977"><tr>
+
+
+
+  <td width="500"><div align="left"><?php $identity = $_REQUEST['id'];
+
+
+
+            echo "<span class='style3'><h1>".CALENDAR."</h1></span>"; ?></div></td>
+
+
+
+  <td width="37">&nbsp;</td>
+
+
+
+  <td width="424"><div align="left"></div></td>
+
+
+
+</tr>
+
+
+
+    <tr>
+
+
+
+      <td colspan="3" class="style1">(Click the drop down to toggle between cabins and coach house availability)</td>
+
+
+
+    </tr>
+
+
+
+    <tr>
+
+
+
+      <td colspan="3" class="style1"><div align="left"><iframe width="700" height="900" scrolling="no" frameborder="0" src="http://www.kincardinecabins.com/calendar/?id_item=2&lang=en"></iframe></div></td>
+
+
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+    <tr>
+
+      <td class="style1">&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+      <td>&nbsp;</td>
+
+  </tr>
+
+
+
+</table>
+
+
---
+++ slideshow.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+slideShow();
+
+function slideShow(){
+    
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex ++;
+    if (slideIndex > slides.length){
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(slideShow, 5000);
+}
